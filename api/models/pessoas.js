be3ed8331904c779b9a4071564992cf7c0d334ad
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 09:43:44 -0300

    cap 2 vid 3 finalizado

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -5,7 +5,15 @@ module.exports = (sequelize, DataTypes) => {
     {
       nome: DataTypes.STRING,
       ativo: DataTypes.BOOLEAN,
-      email: DataTypes.STRING,
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "dado do tipo e-mail inválido"
+          }
+        }
+      },
       role: DataTypes.STRING,
     },
     {
