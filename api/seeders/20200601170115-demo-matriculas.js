19a6bc9215fdd1238971648c7862a830d9bab357
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 13:38:19 -0300

    cap 3 vid 3 finalizado

--- api/seeders/20200601170115-demo-matriculas.js
+++ api/seeders/20200601170115-demo-matriculas.js
@@ -4,42 +4,42 @@ module.exports = {
       {
         status: "confirmado",
         estudante_id: 1,
-        turma_id: 1,
+        turma_id: 9,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         status: "confirmado",
         estudante_id: 2,
-        turma_id: 1,
+        turma_id: 9,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         status: "confirmado",
         estudante_id: 3,
-        turma_id: 2,
+        turma_id: 8,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         status: "confirmado",
         estudante_id: 4,
-        turma_id: 3,
+        turma_id: 8,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         status: "cancelado",
         estudante_id: 1,
-        turma_id: 2,
+        turma_id: 8,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
         status: "cancelado",
-        estudante_id: 2,
-        turma_id: 2,
+        estudante_id: 8,
+        turma_id: 8,
         createdAt: new Date(),
         updatedAt: new Date()
       }
