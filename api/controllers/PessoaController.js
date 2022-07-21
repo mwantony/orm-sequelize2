b3415c4c5d33956b4f482c18367274f25671e9e7
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 13:01:26 -0300

    cap 3 vid 2 finalizado

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -146,7 +146,10 @@ class PessoaController {
   static async pegaMatriculas(req, res) {
     const { estudanteId } = req.params;
     try {
-      const matriculas = await database.Matriculas.findAll({ where: { estudante_id: Number(estudanteId) }});
+      const pessoa = await database.Pessoas.findOne({ where: { id: Number(estudanteId) }});
+      
+      const matriculas = await pessoa.getAulasMatriculadas();
+
       return res.status(200).json(matriculas);
 
     } catch (error) {
