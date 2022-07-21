4c9836cca0ef7f36c2258d2180275fd5f840d254
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 09:50:29 -0300

    cap 2 finalizado

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -3,7 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Pessoas = sequelize.define(
     "Pessoas",
     {
-      nome: DataTypes.STRING,
+      nome: {
+        type: DataTypes.STRING,
+        validate: {
+          funcaoValidadora: function(dado) {
+            if(dado.length < 3) throw new Error("o campo nome deve ter pelo menos 3 caracteres")
+          }
+        }
+      },
       ativo: DataTypes.BOOLEAN,
       email: {
         type: DataTypes.STRING,
