60adff0b18814ad91e7c0c7703491a735baef545
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 16:34:33 -0300

    cap 5 vid 3 finalizado

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,22 +4,36 @@ const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 router
-  .get("/pessoas", PessoaController.pegaPessoasAtivas)
-  .get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas/ativos", PessoaController.pegaPessoasAtivas)
   .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
-  .get("/pessoas/:estudanteId/matricula/:matriculaId",  PessoaController.pegaUmaMatricula)
+  .get(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.pegaUmaMatricula
+  )
   .get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatriculas)
-  .get("/pessoas/matricula/:turmaId/confirmadas", PessoaController.pegaMatriculasPorTurma)
+  .get(
+    "/pessoas/matricula/:turmaId/confirmadas",
+    PessoaController.pegaMatriculasPorTurma
+  )
   .get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas)
   .post("/pessoas", PessoaController.criaPessoa)
   .post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
   .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
-  .post("/pessoas/:estudanteId/matricula/:matriculaId/restaura", PessoaController.restauraMatricula)
+  .post(
+    "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+    PessoaController.restauraMatricula
+  )
   .post("/pessoas/:estudanteId/cancela", PessoaController.cancelaPessoa)
   .put("/pessoas/:id", PessoaController.atualizaPessoa)
-  .put("/pessoas/:estudanteId/matricula/:matriculaId",  PessoaController.atualizaMatricula)
+  .put(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.atualizaMatricula
+  )
   .delete("/pessoas/:id", PessoaController.apagaPessoa)
-  .delete("/pessoas/:estudanteId/matricula/:matriculaId",  PessoaController.apagaMatricula);
-
+  .delete(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.apagaMatricula
+  );
 
 module.exports = router;
--- api/services/PessoasServices.js
+++ api/services/PessoasServices.js
@@ -1,8 +1,18 @@
 const Services = require("../services/Services");
+const database = require("../models");
 
 class PessoasServices extends Services {
   constructor() {
     super("Pessoas");
+  }
+
+  async pegaRegistrosAtivos(where = {}) {
+    return database[this.nomeDoModelo].findAll({ where: { ...where } });
+  }
+  async pegaTodosOsRegistros(where = {}) {
+    return database[this.nomeDoModelo]
+      .scope("todos")
+      .findAll({ where: { ...where } });
   }
 }
 
