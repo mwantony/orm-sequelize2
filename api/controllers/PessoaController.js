e0b1f41ea9434499399ea5b007cfb2dba6ee7fd1
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 15:03:04 -0300

    cap 3 finalizado

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,4 +1,5 @@
 const database = require("../models");
+const Sequelize = require("sequelize")
 
 class PessoaController {
   static async pegaPessoasAtivas(req, res) {
@@ -174,6 +175,23 @@ class PessoaController {
         order: [["estudante_id", "ASC"]]
       });
       return res.status(200).json(todasAsMatriculas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+  static async pegaTurmasLotadas(req, res) {
+    const lotacaoTurma = 2;
+
+    try {
+      const turmasLotadas = await database.Matriculas.findAndCountAll({
+        where: {
+          status: "confirmado"
+        },
+        attributes: ["turma_id"],
+        group: ["turma_id"],
+        having: Sequelize.literal(`COUNT(turma_id) >= ${lotacaoTurma}`)
+      });
+      return res.status(200).json(turmasLotadas.count);
     } catch (error) {
       return res.status(500).json(error.message);
     }
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -7,17 +7,33 @@ router
   .get("/pessoas", PessoaController.pegaPessoasAtivas)
   .get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
   .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
-  .get("/pessoas/:estudanteId/matricula/:matriculaId",  PessoaController.pegaUmaMatricula)
+  .get(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.pegaUmaMatricula
+  )
   .get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatriculas)
-  .get("/pessoas/matricula/:turmaId/confirmadas", PessoaController.pegaMatriculasPorTurma)
+  .get(
+    "/pessoas/matricula/:turmaId/confirmadas",
+    PessoaController.pegaMatriculasPorTurma
+  )
+  .get("/pessoas/matricula/lotada", PessoaController.pegaTurmasLotadas)
   .post("/pessoas", PessoaController.criaPessoa)
   .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
-  .put("/pessoas/:id", PessoaController.atualizaPessoa)
-  .delete("/pessoas/:id", PessoaController.apagaPessoa)
   .post("/pessoas/:estudanteId/matricula", PessoaController.criaMatricula)
-  .put("/pessoas/:estudanteId/matricula/:matriculaId",  PessoaController.atualizaMatricula)
-  .delete("/pessoas/:estudanteId/matricula/:matriculaId",  PessoaController.apagaMatricula)
+  .post(
+    "/pessoas/:estudanteId/matricula/:matriculaId/restaura",
+    PessoaController.restauraMatricula
+  )
   .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
-  .post("/pessoas/:estudanteId/matricula/:matriculaId/restaura", PessoaController.restauraMatricula);
+  .delete(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.apagaMatricula
+  )
+  .delete("/pessoas/:id", PessoaController.apagaPessoa)
+  .put("/pessoas/:id", PessoaController.atualizaPessoa)
+  .put(
+    "/pessoas/:estudanteId/matricula/:matriculaId",
+    PessoaController.atualizaMatricula
+  );
 
 module.exports = router;
