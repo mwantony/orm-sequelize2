a721741f3d8b9c20748b2b83f94a2dc860b2328c
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-21 09:20:24 -0300

    cap 2 vid 1 finalizado

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,19 +1,27 @@
-"use strict"
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Pessoas = sequelize.define("Pessoas", {
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, { paranoid: true })
-  Pessoas.associate = function(models) {
+  const Pessoas = sequelize.define(
+    "Pessoas",
+    {
+      nome: DataTypes.STRING,
+      ativo: DataTypes.BOOLEAN,
+      email: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      paranoid: true,
+      defaultScope: {
+        where: { ativo: true },
+      },
+    }
+  );
+  Pessoas.associate = function (models) {
     Pessoas.hasMany(models.Turmas, {
-      foreignKey: "docente_id"
-    }) 
+      foreignKey: "docente_id",
+    });
     Pessoas.hasMany(models.Matriculas, {
-      foreignKey: "estudante_id"
-    })
-
-  }
-  return Pessoas
-}
+      foreignKey: "estudante_id",
+    });
+  };
+  return Pessoas;
+};
